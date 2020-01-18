c5bfe0383e6e822060b12843153b2273931b27c0
Daniel Morrison <dan@morrison.ph>
2020-01-18 18:42:06 -0500

    Add simple Pi Pad calculator

--- PadCalculator.js
+++ PadCalculator.js
@@ -9,7 +9,28 @@
         let lin = Math.sqrt(large / small) + Math.sqrt(large / small - 1);
         return Lin2dB(lin);
     }
+
+    function GetPiPad(attenuation, zIn, zOut) {
+        let vOut = Math.sqrt((zOut / zIn) * (1 / Math.pow(10, attenuation / 10)));
+
+        let shuntInN = (zOut * zIn) - (Math.pow(zIn, 2) * Math.pow(vOut, 2));
+        let shuntInD = zOut + (zIn * Math.pow(vOut, 2)) - 2 * zIn * vOut;
+        let shuntIn = shuntInN / shuntInD;
+
+
+        let shuntOut = vOut / (1 / zIn - vOut / zOut - 1 / shuntIn);
+
+        let series = (1 - vOut) * zIn * shuntIn / (shuntIn - zIn);
+
+        return {
+            shuntIn,
+            series,
+            shuntOut,
+        };
+    }
+
     window.PadCalculator = {
         GetMinimumMatchAttenuation,
+        GetPiPad,
     }
 }())
--- PadCalculatorSpec.js
+++ PadCalculatorSpec.js
@@ -4,4 +4,11 @@ describe('An attenuator calculator', () => {
         expect(PadCalculator.GetMinimumMatchAttenuation(45, 50)).toBeCloseTo(2.844, 3);
         expect(PadCalculator.GetMinimumMatchAttenuation(25, 100)).toBeCloseTo(11.439, 3);
     });
+
+    it('should calculate simple pi pads', () => {
+        let pad3Db = PadCalculator.GetPiPad(3, 50, 50)
+        expect(pad3Db.shuntIn).toBeCloseTo(292.402, 3);
+        expect(pad3Db.series).toBeCloseTo(17.615, 3);
+        expect(pad3Db.shuntOut).toBeCloseTo(pad3Db.shuntIn);
+    });
 })
