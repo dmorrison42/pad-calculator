1624b3bf3dd683d5bb8142f972017052b3a1cb43
Daniel Morrison <dan@morrison.ph>
2020-01-24 20:45:52 -0500

    Refactor to use module

--- PadCalculator.js
+++ PadCalculator.js
@@ -1,95 +1,80 @@
-/* global PadCalculator */
-(function () {
-  var resistorInfo = null
+var resistorInfo = null
 
-  var resistorInfoPromise = new Promise((resolve, reject) => {
-    var request = new window.XMLHttpRequest()
-    request.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        resolve(JSON.parse(this.responseText))
+var resistorInfoPromise = new Promise((resolve, reject) => {
+  var request = new window.XMLHttpRequest()
+  request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      resolve(JSON.parse(this.responseText))
+    }
+  }
+  request.open('GET', 'ResistorValues.json')
+  request.send()
+})
+
+resistorInfoPromise.then(data => { resistorInfo = data })
+
+const LoadPromise = resistorInfoPromise.then()
+export { LoadPromise }
+
+export class PiPad {
+  constructor (shuntIn, series, shuntOut, circuitZIn, circuitZOut) {
+    this.shuntIn = shuntIn
+    this.series = series
+    this.shuntOut = shuntOut
+    this.circuitZIn = circuitZIn
+    this.circuitZOut = circuitZOut
+
+    if (circuitZIn == null && circuitZOut == null) {
+      let x = 10000
+      let y = 10000
+      let result = {}
+      for (let i = 1; i < 100; i++) {
+        result = new PiPad(shuntIn, series, shuntOut, x, y)
+        x = result.zIn
+        y = result.zOut
       }
-    }
-    request.open('GET', 'ResistorValues.json')
-    request.send()
-  })
-
-  resistorInfoPromise.then(data => { resistorInfo = data })
-
-  class PiPad {
-    constructor (shuntIn, series, shuntOut, circuitZIn, circuitZOut) {
-      this.shuntIn = shuntIn
-      this.series = series
-      this.shuntOut = shuntOut
-      this.circuitZIn = circuitZIn
-      this.circuitZOut = circuitZOut
-
-      if (circuitZIn == null && circuitZOut == null) {
-        let x = 10000
-        let y = 10000
-        let result = {}
-        for (let i = 1; i < 100; i++) {
-          result = new PiPad(shuntIn, series, shuntOut, x, y)
-          x = result.zIn
-          y = result.zOut
-        }
-        this.circuitZIn = x
-        this.circuitZOut = y
-      }
-    }
-
-    get zIn () {
-      return calculatePiImpedance(
-        this.shuntIn, this.series, this.shuntOut, this.circuitZOut)
-    }
-
-    get zOut () {
-      return calculatePiImpedance(
-        this.shuntOut, this.series, this.shuntIn, this.circuitZIn)
-    }
-
-    get vswrIn () {
-      return calculateVSWR(this.circuitZIn, this.zIn)
-    }
-
-    get vswrOut () {
-      return calculateVSWR(this.circuitZOut, this.zOut)
-    }
-
-    get returnLossIn () {
-      return calculateReturnLoss(this.circuitZIn, this.zIn)
-    }
-
-    get returnLossOut () {
-      return calculateReturnLoss(this.circuitZOut, this.zOut)
-    }
-
-    get attenuationForward () {
-      return calculatePiAttenuation(
-        this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut)
-    }
-
-    get attenuationReverse () {
-      return calculatePiAttenuation(
-        this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut)
+      this.circuitZIn = x
+      this.circuitZOut = y
     }
   }
 
-  function v2dB (x) {
-    return 20 * Math.log10(x)
+  get zIn () {
+    return calculatePiImpedance(
+      this.shuntIn, this.series, this.shuntOut, this.circuitZOut)
   }
 
-  function power2dB (x) {
-    return 10 * Math.log10(x)
+  get zOut () {
+    return calculatePiImpedance(
+      this.shuntOut, this.series, this.shuntIn, this.circuitZIn)
   }
 
-  function GetMinimumMatchAttenuation (zIn, zOut) {
-    const small = Math.min(zIn, zOut)
-    const large = Math.max(zIn, zOut)
-    const lin = Math.sqrt(large / small) + Math.sqrt(large / small - 1)
-    return v2dB(lin)
+  get vswrIn () {
+    return calculateVSWR(this.circuitZIn, this.zIn)
   }
 
-  function GetPiPad (attenuation, zIn, zOut) {
+  get vswrOut () {
+    return calculateVSWR(this.circuitZOut, this.zOut)
+  }
+
+  get returnLossIn () {
+    return calculateReturnLoss(this.circuitZIn, this.zIn)
+  }
+
+  get returnLossOut () {
+    return calculateReturnLoss(this.circuitZOut, this.zOut)
+  }
+
+  get attenuationForward () {
+    return calculatePiAttenuation(
+      this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut)
+  }
+
+  get attenuationReverse () {
+    return calculatePiAttenuation(
+      this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut)
+  }
+
+  static get (attenuation, zIn, zOut) {
     zIn = zIn == null ? 50 : zIn
     zOut = zOut == null ? 50 : zOut
 
@@ -106,49 +91,10 @@
     return new PiPad(shuntIn, series, shuntOut, zIn, zOut)
   }
 
-  function calculatePiImpedance (a, b, c, zC) {
-    const n = a * (b * zC + b * c + c * zC)
-    const d = c * zC + ((b + a) * (zC + c))
-    return n / d
-  }
-
-  function calculatePiAttenuation (a, b, c, zA, zC) {
-    const zACalculated = calculatePiImpedance(a, b, c, zC)
-    const vIn = 2 - ((2 * zA) / (zA + zACalculated))
-    const vOut = (vIn * c * zC) / (b * (zC + c) + c * zC)
-    return power2dB((1 / zA) / (Math.pow(vOut, 2) / zC))
-  }
-
-  function calculateVSWR (z, calc) {
-    const a = ((1 + (calc - z) / (calc + z)) / (1 - (calc - z) / (calc + z)))
-    const b = ((1 - (calc - z) / (calc + z)) / (1 + (calc - z) / (calc + z)))
-    return Math.max(a, b)
-  }
-
-  function calculateReturnLoss (z, calc) {
-    return v2dB(Math.abs((calc - z) / (calc + z)))
-  }
-
-  function GetNearestValues (series, exact) {
-    const values = resistorInfo.Series[series].Values
-    let low = 0
-
-    for (var i = Math.floor(Math.log10(exact)) - 1; ; i++) {
-      const mag = Math.pow(10, i)
-      for (var j = -1; j < values.length; j++) {
-        const value = mag * values[j]
-        if (value <= exact) low = value
-        if (value >= exact) {
-          return [low, value]
-        }
-      }
-    }
-  }
-
-  function GetPadsInSeries (resistorSeries, attenuation) {
+  static getInSeries (resistorSeries, attenuation, zIn, zOut) {
     // TODO: Do more searching around
     const nearest = (v) => GetNearestValues(resistorSeries, v)
-    const ideal = PadCalculator.GetPiPad(attenuation)
+    const ideal = this.get(attenuation, zIn, zOut)
     const shuntIn = nearest(ideal.shuntIn)
     const shuntOut = nearest(ideal.shuntOut)
     const series = nearest(ideal.series)
@@ -169,11 +115,60 @@
     return pads
   }
 
-  window.PadCalculator = {
-    LoadPromise: resistorInfoPromise.then(),
-    GetMinimumMatchAttenuation,
-    GetPiPad,
-    PiPad,
-    GetPadsInSeries
+  static attenuationRequired (zIn, zOut) {
+    const small = Math.min(zIn, zOut)
+    const large = Math.max(zIn, zOut)
+    const lin = Math.sqrt(large / small) + Math.sqrt(large / small - 1)
+    return v2dB(lin)
   }
-})()
+}
+
+function v2dB (x) {
+  return 20 * Math.log10(x)
+}
+
+function power2dB (x) {
+  return 10 * Math.log10(x)
+}
+
+export function GetMinimumMatchAttenuation (zIn, zOut) {
+}
+
+function calculatePiImpedance (a, b, c, zC) {
+  const n = a * (b * zC + b * c + c * zC)
+  const d = c * zC + ((b + a) * (zC + c))
+  return n / d
+}
+
+function calculatePiAttenuation (a, b, c, zA, zC) {
+  const zACalculated = calculatePiImpedance(a, b, c, zC)
+  const vIn = 2 - ((2 * zA) / (zA + zACalculated))
+  const vOut = (vIn * c * zC) / (b * (zC + c) + c * zC)
+  return power2dB((1 / zA) / (Math.pow(vOut, 2) / zC))
+}
+
+function calculateVSWR (z, calc) {
+  const a = ((1 + (calc - z) / (calc + z)) / (1 - (calc - z) / (calc + z)))
+  const b = ((1 - (calc - z) / (calc + z)) / (1 + (calc - z) / (calc + z)))
+  return Math.max(a, b)
+}
+
+function calculateReturnLoss (z, calc) {
+  return v2dB(Math.abs((calc - z) / (calc + z)))
+}
+
+function GetNearestValues (series, exact) {
+  const values = resistorInfo.Series[series].Values
+  let low = 0
+
+  for (var i = Math.floor(Math.log10(exact)) - 1; ; i++) {
+    const mag = Math.pow(10, i)
+    for (var j = -1; j < values.length; j++) {
+      const value = mag * values[j]
+      if (value <= exact) low = value
+      if (value >= exact) {
+        return [low, value]
+      }
+    }
+  }
+}
