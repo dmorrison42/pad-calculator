dcb243d3ce6946930771bbfbd867d41513cca108
Daniel Morrison <dan@morrison.ph>
2020-01-24 20:22:13 -0500

    Use standard style
    Yay tests, cause that was thrash...

--- PadCalculator.js
+++ PadCalculator.js
@@ -1,180 +1,179 @@
-!(function () {
-    var resistorInfo = null;
+/* global PadCalculator */
+(function () {
+  var resistorInfo = null
 
-    var resistorInfoPromise = new Promise((resolve, reject) => {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                resolve(JSON.parse(this.responseText));
-            }
+  var resistorInfoPromise = new Promise((resolve, reject) => {
+    var request = new window.XMLHttpRequest()
+    request.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        resolve(JSON.parse(this.responseText))
+      }
+    }
+    request.open('GET', 'ResistorValues.json')
+    request.send()
+  })
+
+  resistorInfoPromise.then(data => { resistorInfo = data })
+
+  class PiPad {
+    constructor (shuntIn, series, shuntOut, circuitZIn, circuitZOut) {
+      this.shuntIn = shuntIn
+      this.series = series
+      this.shuntOut = shuntOut
+      this.circuitZIn = circuitZIn
+      this.circuitZOut = circuitZOut
+
+      if (circuitZIn == null && circuitZOut == null) {
+        let x = 10000
+        let y = 10000
+        let result = {}
+        for (let i = 1; i < 100; i++) {
+          result = new PiPad(shuntIn, series, shuntOut, x, y)
+          x = result.zIn
+          y = result.zOut
         }
-        request.open('GET', 'ResistorValues.json');
-        request.send();
-    });
-
-    resistorInfoPromise.then(data => resistorInfo = data);
-
-    class PiPad {
-        constructor(shuntIn, series, shuntOut, circuitZIn, circuitZOut) {
-            this.shuntIn = shuntIn;
-            this.series = series;
-            this.shuntOut = shuntOut;
-            this.circuitZIn = circuitZIn;
-            this.circuitZOut = circuitZOut;
-
-            if (circuitZIn == null && circuitZOut == null) {
-                let x = 10000;
-                let y = 10000;
-                let result = {};
-                for (let i = 1; i < 100; i++) {
-                    result = new PiPad(shuntIn, series, shuntOut, x, y);
-                    x = result.zIn;
-                    y = result.zOut;
-                }
-                this.circuitZIn = x;
-                this.circuitZOut = y;
-            }
-
-        }
-
-        get zIn() {
-            return calculatePiImpedance(
-                this.shuntIn, this.series, this.shuntOut, this.circuitZOut);
-        }
-
-        get zOut() {
-            return calculatePiImpedance(
-                this.shuntOut, this.series, this.shuntIn, this.circuitZIn);
-        }
-
-        get vswrIn() {
-            return calculateVSWR(this.circuitZIn, this.zIn);
-        }
-
-        get vswrOut() {
-            return calculateVSWR(this.circuitZOut, this.zOut);
-        }
-
-        get returnLossIn() {
-            return calculateReturnLoss(this.circuitZIn, this.zIn);
-        }
-
-        get returnLossOut() {
-            return calculateReturnLoss(this.circuitZOut, this.zOut);
-        }
-
-        get attenuationForward() {
-            return calculatePiAttenuation(
-                this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut);
-        }
-
-        get attenuationReverse() {
-            return calculatePiAttenuation(
-                this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut);
-        }
+        this.circuitZIn = x
+        this.circuitZOut = y
+      }
     }
 
-    function v2dB(x) {
-        return 20 * Math.log10(x);
+    get zIn () {
+      return calculatePiImpedance(
+        this.shuntIn, this.series, this.shuntOut, this.circuitZOut)
     }
 
-    function power2dB(x) {
-        return 10 * Math.log10(x);
+    get zOut () {
+      return calculatePiImpedance(
+        this.shuntOut, this.series, this.shuntIn, this.circuitZIn)
     }
 
-    function GetMinimumMatchAttenuation(zIn, zOut) {
-        let small = Math.min(zIn, zOut);
-        let large = Math.max(zIn, zOut);
-        let lin = Math.sqrt(large / small) + Math.sqrt(large / small - 1);
-        return v2dB(lin);
+    get vswrIn () {
+      return calculateVSWR(this.circuitZIn, this.zIn)
     }
 
-    function GetPiPad(attenuation, zIn, zOut) {
-        zIn = zIn == null ? 50 : zIn;
-        zOut = zOut == null ? 50 : zOut;
-
-        let vOut = Math.sqrt((zOut / zIn) * (1 / Math.pow(10, attenuation / 10)));
-
-        let shuntInN = (zOut * zIn) - (Math.pow(zIn, 2) * Math.pow(vOut, 2));
-        let shuntInD = zOut + (zIn * Math.pow(vOut, 2)) - 2 * zIn * vOut;
-        let shuntIn = shuntInN / shuntInD;
-
-
-        let shuntOut = vOut / (1 / zIn - vOut / zOut - 1 / shuntIn);
-
-        let series = (1 - vOut) * zIn * shuntIn / (shuntIn - zIn);
-
-        return new PiPad(shuntIn, series, shuntOut, zIn, zOut);
+    get vswrOut () {
+      return calculateVSWR(this.circuitZOut, this.zOut)
     }
 
-    function calculatePiImpedance(a, b, c, zC) {
-        let n = a * (b * zC + b * c + c * zC);
-        let d = c * zC + ((b + a) * (zC + c));
-        return n / d;
+    get returnLossIn () {
+      return calculateReturnLoss(this.circuitZIn, this.zIn)
     }
 
-    function calculatePiAttenuation(a, b, c, zA, zC) {
-        let zACalculated = calculatePiImpedance(a, b, c, zC);
-        let vIn = 2 - ((2 * zA) / (zA + zACalculated))
-        let vOut = (vIn * c * zC) / (b * (zC + c) + c * zC)
-        return power2dB((1 / zA) / (Math.pow(vOut, 2) / zC));
+    get returnLossOut () {
+      return calculateReturnLoss(this.circuitZOut, this.zOut)
     }
 
-    function calculateVSWR(z, calc) {
-        let a = ((1 + (calc - z) / (calc + z)) / (1 - (calc - z) / (calc + z)));
-        let b = ((1 - (calc - z) / (calc + z)) / (1 + (calc - z) / (calc + z)));
-        return Math.max(a, b);
+    get attenuationForward () {
+      return calculatePiAttenuation(
+        this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut)
     }
 
-    function calculateReturnLoss(z, calc) {
-        return v2dB(Math.abs((calc - z) / (calc + z)));
+    get attenuationReverse () {
+      return calculatePiAttenuation(
+        this.shuntIn, this.series, this.shuntOut, this.circuitZIn, this.circuitZOut)
     }
+  }
 
-    function GetNearestValues(series, exact) {
-        let values = resistorInfo.Series[series].Values;
-        let low = 0;
+  function v2dB (x) {
+    return 20 * Math.log10(x)
+  }
 
-        for (var i = Math.floor(Math.log10(exact)) - 1; ; i++) {
-            let mag = Math.pow(10, i);
-            for (var j = -1; j < values.length; j++) {
-                let value = mag * values[j];
-                if (value <= exact) low = value;
-                if (value >= exact) {
-                    return [low, value];
-                }
-            }
+  function power2dB (x) {
+    return 10 * Math.log10(x)
+  }
+
+  function GetMinimumMatchAttenuation (zIn, zOut) {
+    const small = Math.min(zIn, zOut)
+    const large = Math.max(zIn, zOut)
+    const lin = Math.sqrt(large / small) + Math.sqrt(large / small - 1)
+    return v2dB(lin)
+  }
+
+  function GetPiPad (attenuation, zIn, zOut) {
+    zIn = zIn == null ? 50 : zIn
+    zOut = zOut == null ? 50 : zOut
+
+    const vOut = Math.sqrt((zOut / zIn) * (1 / Math.pow(10, attenuation / 10)))
+
+    const shuntInN = (zOut * zIn) - (Math.pow(zIn, 2) * Math.pow(vOut, 2))
+    const shuntInD = zOut + (zIn * Math.pow(vOut, 2)) - 2 * zIn * vOut
+    const shuntIn = shuntInN / shuntInD
+
+    const shuntOut = vOut / (1 / zIn - vOut / zOut - 1 / shuntIn)
+
+    const series = (1 - vOut) * zIn * shuntIn / (shuntIn - zIn)
+
+    return new PiPad(shuntIn, series, shuntOut, zIn, zOut)
+  }
+
+  function calculatePiImpedance (a, b, c, zC) {
+    const n = a * (b * zC + b * c + c * zC)
+    const d = c * zC + ((b + a) * (zC + c))
+    return n / d
+  }
+
+  function calculatePiAttenuation (a, b, c, zA, zC) {
+    const zACalculated = calculatePiImpedance(a, b, c, zC)
+    const vIn = 2 - ((2 * zA) / (zA + zACalculated))
+    const vOut = (vIn * c * zC) / (b * (zC + c) + c * zC)
+    return power2dB((1 / zA) / (Math.pow(vOut, 2) / zC))
+  }
+
+  function calculateVSWR (z, calc) {
+    const a = ((1 + (calc - z) / (calc + z)) / (1 - (calc - z) / (calc + z)))
+    const b = ((1 - (calc - z) / (calc + z)) / (1 + (calc - z) / (calc + z)))
+    return Math.max(a, b)
+  }
+
+  function calculateReturnLoss (z, calc) {
+    return v2dB(Math.abs((calc - z) / (calc + z)))
+  }
+
+  function GetNearestValues (series, exact) {
+    const values = resistorInfo.Series[series].Values
+    let low = 0
+
+    for (var i = Math.floor(Math.log10(exact)) - 1; ; i++) {
+      const mag = Math.pow(10, i)
+      for (var j = -1; j < values.length; j++) {
+        const value = mag * values[j]
+        if (value <= exact) low = value
+        if (value >= exact) {
+          return [low, value]
         }
+      }
     }
+  }
 
-    function GetPadsInSeries(resistorSeries, attenuation) {
-        // TODO: Do more searching around
-        let nearest = (v) => GetNearestValues(resistorSeries, v)
-        let ideal = PadCalculator.GetPiPad(attenuation);
-        let shuntIn = nearest(ideal.shuntIn);
-        let shuntOut = nearest(ideal.shuntOut);
-        let series = nearest(ideal.series);
+  function GetPadsInSeries (resistorSeries, attenuation) {
+    // TODO: Do more searching around
+    const nearest = (v) => GetNearestValues(resistorSeries, v)
+    const ideal = PadCalculator.GetPiPad(attenuation)
+    const shuntIn = nearest(ideal.shuntIn)
+    const shuntOut = nearest(ideal.shuntOut)
+    const series = nearest(ideal.series)
 
-        let pads = [];
-        for (var i = 0; i < shuntIn.length; i++) {
-            for (var j = 0; j < series.length; j++) {
-                for (var k = 0; k < shuntOut.length; k++) {
-                    pads.push({
-                        shuntIn: shuntIn[i],
-                        series: series[j],
-                        shuntOut: shuntOut[k],
-                    })
-                }
-            }
+    const pads = []
+    for (var i = 0; i < shuntIn.length; i++) {
+      for (var j = 0; j < series.length; j++) {
+        for (var k = 0; k < shuntOut.length; k++) {
+          pads.push({
+            shuntIn: shuntIn[i],
+            series: series[j],
+            shuntOut: shuntOut[k]
+          })
         }
-
-        return pads;
+      }
     }
 
-    window.PadCalculator = {
-        LoadPromise: resistorInfoPromise.then(),
-        GetMinimumMatchAttenuation,
-        GetPiPad,
-        PiPad,
-        GetPadsInSeries,
-    }
-}())
+    return pads
+  }
+
+  window.PadCalculator = {
+    LoadPromise: resistorInfoPromise.then(),
+    GetMinimumMatchAttenuation,
+    GetPiPad,
+    PiPad,
+    GetPadsInSeries
+  }
+})()
--- PadCalculatorSpec.js
+++ PadCalculatorSpec.js
@@ -1,72 +1,74 @@
+/* eslint-env jasmine */
+/* global PadCalculator */
 describe('An attenuator calculator', () => {
-    it('should calculate match attenuation', () => {
-        expect(PadCalculator.GetMinimumMatchAttenuation(50, 50)).toBe(0);
-        expect(PadCalculator.GetMinimumMatchAttenuation(45, 50)).toBeCloseTo(2.844, 3);
-        expect(PadCalculator.GetMinimumMatchAttenuation(25, 100)).toBeCloseTo(11.439, 3);
-    });
+  it('should calculate match attenuation', () => {
+    expect(PadCalculator.GetMinimumMatchAttenuation(50, 50)).toBe(0)
+    expect(PadCalculator.GetMinimumMatchAttenuation(45, 50)).toBeCloseTo(2.844, 3)
+    expect(PadCalculator.GetMinimumMatchAttenuation(25, 100)).toBeCloseTo(11.439, 3)
+  })
 
-    it('should calculate simple pi pads', () => {
-        let pad3Db = PadCalculator.GetPiPad(3, 50, 50)
-        expect(pad3Db.shuntIn).toBeCloseTo(292.402, 3);
-        expect(pad3Db.series).toBeCloseTo(17.615, 3);
-        expect(pad3Db.shuntOut).toBeCloseTo(pad3Db.shuntIn, 3);
-    });
+  it('should calculate simple pi pads', () => {
+    const pad3Db = PadCalculator.GetPiPad(3, 50, 50)
+    expect(pad3Db.shuntIn).toBeCloseTo(292.402, 3)
+    expect(pad3Db.series).toBeCloseTo(17.615, 3)
+    expect(pad3Db.shuntOut).toBeCloseTo(pad3Db.shuntIn, 3)
+  })
 
-    it('should assume 50 ohms', () => {
-        let pad3Db = PadCalculator.GetPiPad(3)
-        expect(pad3Db.shuntIn).toBeCloseTo(292.402, 3);
-        expect(pad3Db.series).toBeCloseTo(17.615, 3);
-        expect(pad3Db.shuntOut).toBeCloseTo(pad3Db.shuntIn, 3);
-    })
+  it('should assume 50 ohms', () => {
+    const pad3Db = PadCalculator.GetPiPad(3)
+    expect(pad3Db.shuntIn).toBeCloseTo(292.402, 3)
+    expect(pad3Db.series).toBeCloseTo(17.615, 3)
+    expect(pad3Db.shuntOut).toBeCloseTo(pad3Db.shuntIn, 3)
+  })
 
-    it('should calculate unmatched pi pads', () => {
-        let pad6Db = PadCalculator.GetPiPad(6, 47, 55)
-        expect(pad6Db.shuntIn).toBeCloseTo(108.431, 3);
-        expect(pad6Db.series).toBeCloseTo(37.982, 3);
-        expect(pad6Db.shuntOut).toBeCloseTo(246.827, 3);
-    });
+  it('should calculate unmatched pi pads', () => {
+    const pad6Db = PadCalculator.GetPiPad(6, 47, 55)
+    expect(pad6Db.shuntIn).toBeCloseTo(108.431, 3)
+    expect(pad6Db.series).toBeCloseTo(37.982, 3)
+    expect(pad6Db.shuntOut).toBeCloseTo(246.827, 3)
+  })
 
-    it('should evaluate pi pads', () => {
-        let samplePad = new PadCalculator.PiPad(50, 200, 30, 50, 50);
-        expect(samplePad.zIn).toBeCloseTo(40.698, 3);
-        expect(samplePad.zOut).toBeCloseTo(26.471, 3);
-        expect(samplePad.attenuationForward).toBeCloseTo(22.28, 2);
-        expect(samplePad.attenuationReverse).toBeCloseTo(22.28, 2);
-    });
+  it('should evaluate pi pads', () => {
+    const samplePad = new PadCalculator.PiPad(50, 200, 30, 50, 50)
+    expect(samplePad.zIn).toBeCloseTo(40.698, 3)
+    expect(samplePad.zOut).toBeCloseTo(26.471, 3)
+    expect(samplePad.attenuationForward).toBeCloseTo(22.28, 2)
+    expect(samplePad.attenuationReverse).toBeCloseTo(22.28, 2)
+  })
 
-    it('should return the inputs given outputs', () => {
-        let samplePad = new PadCalculator.PiPad(150.476, 37.352, 150.476);
-        expect(samplePad.zIn).toBeCloseTo(50, 3);
-        expect(samplePad.zOut).toBeCloseTo(50, 3);
-        expect(samplePad.attenuationForward).toBeCloseTo(6, 3);
-        expect(samplePad.attenuationReverse).toBeCloseTo(6, 3);
-    });
+  it('should return the inputs given outputs', () => {
+    const samplePad = new PadCalculator.PiPad(150.476, 37.352, 150.476)
+    expect(samplePad.zIn).toBeCloseTo(50, 3)
+    expect(samplePad.zOut).toBeCloseTo(50, 3)
+    expect(samplePad.attenuationForward).toBeCloseTo(6, 3)
+    expect(samplePad.attenuationReverse).toBeCloseTo(6, 3)
+  })
 
-    it('should return the inputs given outputs with mismatch', () => {
-        let samplePad = new PadCalculator.PiPad(86.517, 45.747, 2386.203);
-        expect(samplePad.zIn).toBeCloseTo(50, 3);
-        expect(samplePad.zOut).toBeCloseTo(75, 3);
-        expect(samplePad.attenuationForward).toBeCloseTo(6, 3);
-        expect(samplePad.attenuationReverse).toBeCloseTo(6, 3);
-    });
+  it('should return the inputs given outputs with mismatch', () => {
+    const samplePad = new PadCalculator.PiPad(86.517, 45.747, 2386.203)
+    expect(samplePad.zIn).toBeCloseTo(50, 3)
+    expect(samplePad.zOut).toBeCloseTo(75, 3)
+    expect(samplePad.attenuationForward).toBeCloseTo(6, 3)
+    expect(samplePad.attenuationReverse).toBeCloseTo(6, 3)
+  })
 
-    it('should calculate VSWR', () => {
-        let samplePad = new PadCalculator.PiPad(50, 200, 30, 50, 75);
+  it('should calculate VSWR', () => {
+    const samplePad = new PadCalculator.PiPad(50, 200, 30, 50, 75)
 
-        expect(samplePad.vswrIn).toBeCloseTo(1.226, 3);
-        expect(samplePad.vswrOut).toBeCloseTo(2.833, 3);
-    });
+    expect(samplePad.vswrIn).toBeCloseTo(1.226, 3)
+    expect(samplePad.vswrOut).toBeCloseTo(2.833, 3)
+  })
 
-    it('should calculate return loss', () => {
-        let samplePad = new PadCalculator.PiPad(50, 200, 30, 50, 75);
+  it('should calculate return loss', () => {
+    const samplePad = new PadCalculator.PiPad(50, 200, 30, 50, 75)
 
-        expect(samplePad.returnLossIn).toBeCloseTo(-19.875, 3);
-        expect(samplePad.returnLossOut).toBeCloseTo(-6.407, 3);
-    });
+    expect(samplePad.returnLossIn).toBeCloseTo(-19.875, 3)
+    expect(samplePad.returnLossOut).toBeCloseTo(-6.407, 3)
+  })
 
-    it('should search for close real pads', async () => {
-        await PadCalculator.LoadPromise;
-        let samplePads = PadCalculator.GetPadsInSeries('E24', 3);
-        expect(samplePads.length).toBeGreaterThan(0);
-    });
+  it('should search for close real pads', async () => {
+    await PadCalculator.LoadPromise
+    const samplePads = PadCalculator.GetPadsInSeries('E24', 3)
+    expect(samplePads.length).toBeGreaterThan(0)
+  })
 })
