9d56068b0d9c1ff6e616a1cd6e45863adc3b0863
Daniel Morrison <dan@morrison.ph>
2020-02-24 20:28:30 -0500

    Hovertext showing tolerances

--- PadList.js
+++ PadList.js
@@ -1,5 +1,5 @@
 /* global Vue */
-import { PiPad } from './PadCalculator.js'
+import { PiPad, resistorInfo } from './PadCalculator.js'
 
 const places = 2
 
@@ -17,19 +17,9 @@ export const PadList = Vue.component('pad-list', {
     },
     round (value) {
       return value.toFixed(places)
-    }
-  },
-  computed: {
-    calculatorPads () {
-      return [this.pad, ...PiPad.getInSeries(
-        this.series,
-        // Pretty sure this is always the same when considering reflection
-        this.pad.attenuationForward,
-        this.pad.circuitZIn,
-        this.pad.circuitZOut)]
     },
-    formated (pad) {
-      return this.calculatorPads.map(pad => ({
+    format (pad) {
+      return {
         'Shunt In': this.round(pad.shuntIn),
         Series: this.round(pad.series),
         'Shunt Out': this.round(pad.shuntOut),
@@ -42,15 +32,55 @@ export const PadList = Vue.component('pad-list', {
         'Output Return Loss': this.cap(pad.returnLossOut, -50),
         'Input VSWR': this.round(pad.vswrIn),
         'Output VSWR': this.round(pad.vswrOut)
-      }))
+      }
+    },
+    ranges (pad, key) {
+      return this.tolerances.map((tolerance) => {
+        const multipliers = [(100 - tolerance) / 100, (100 + tolerance) / 100]
+        const allThePads = [pad]
+
+        for (var i = 0; i < multipliers.length; i++) {
+          for (var j = 0; j < multipliers.length; j++) {
+            for (var k = 0; k < multipliers.length; k++) {
+              allThePads.push(new PiPad(
+                multipliers[i] * pad.shuntIn,
+                multipliers[j] * pad.series,
+                multipliers[k] * pad.shuntOut))
+            }
+          }
+        }
+
+        const allValues = allThePads
+          .map(this.format)
+          .map(v => v[key])
+          .sort()
+
+        const min = allValues[0]
+        const max = allValues[allValues.length - 1]
+
+        return `${tolerance}% ${min}-${max}`
+      }).join('\n')
+    }
+  },
+  computed: {
+    calculatorPads () {
+      return [this.pad, ...PiPad.getInSeries(
+        this.series,
+        // Pretty sure this is always the same when considering reflection
+        this.pad.attenuationForward,
+        this.pad.circuitZIn,
+        this.pad.circuitZOut)]
+    },
+    tolerances () {
+      return resistorInfo.Series[this.series].Tolerances
     }
   },
   template: `<table>
   <tr>
     <th v-for="key in keys" :key="key">{{key}}</th>
   </tr>
-  <tr v-for="pad in formated">
-    <td v-for="key in keys" :key="key" align="right">{{pad[key]}}</td>
+  <tr v-for="pad in calculatorPads">
+    <td v-for="key in keys" :key="key" align="right" :title="ranges(pad, key)">{{format(pad)[key]}}</td>
   </tr>
 </table>`
 })
